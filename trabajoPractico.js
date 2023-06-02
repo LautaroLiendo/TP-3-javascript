5d633fe9e4c008437ddbd962d4c8817a187672da
lautaroliendo <liendolautaro@gmail.com>
2023-06-02 04:44:30 -0300

    ejercicio 1 realizado

---
+++ trabajoPractico.js
@@ -0,0 +1,16 @@
+// 1- Crear un array llamado meses y que almacene el nombre de los doce meses del año. Mostrar por pantalla en forma de lista los doce nombres del arreglo.
+
+// const meses=["enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre"];
+
+// console.log(meses);
+
+
+
+let meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+for (const mes of meses) {//ejecuta un bloque de código para cada elemento de un objeto iterable
+    console.log(mes);
+}
+
+//2--------------------------------------------------------------------------------------------------
+
