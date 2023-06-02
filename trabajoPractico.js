a4ddb87bb91254ae8f17c24f04c81aa529b3242d
lautaroliendo <liendolautaro@gmail.com>
2023-06-02 05:30:09 -0300

    ejercicio 2 realizado

--- trabajoPractico.js
+++ trabajoPractico.js
@@ -6,11 +6,46 @@
 
 
 
-let meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+// let meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 
-for (const mes of meses) {//ejecuta un bloque de código para cada elemento de un objeto iterable
-    console.log(mes);
-}
+// for (const mes of meses) {//ejecuta un bloque de código para cada elemento de un objeto iterable
+//     console.log(mes);
+// }
 
 //2--------------------------------------------------------------------------------------------------
+// 2-  Crear un script que solicite al usuario mediante un prompt el nombre de ciudades y almacenarlas en un arreglo, cuando el usuario selecciona cancelar se debe mostrar el arreglo generado, luego realizar las siguientes acciones:
+
+// Mostrar la longitud del arreglo.
+// Mostrar en el documento web los ítems de las posiciones primera, tercera y última.
+// Añade en última posición la ciudad de París.
+// Escribe por pantalla el elemento que ocupa la segunda posición.
+// Sustituye el elemento que ocupa la segunda posición por la ciudad de 'Barcelona'.
+
+// let ciudades=[];
+
+// while (true) {
+//     let pedir=prompt("escribe varios nombres de ciudades");
+    
+//     if (pedir==null) {break;
+//     }
+//     ciudades.push(pedir);
+// }
+
+// ciudades[1]="barcelona"
+// ciudades.push("Paris");
+// document.write(ciudades);
+
+// document.write("<br>");
+// document.write("la longitud del array es"+" "+ciudades.length);
+// document.write("<br>");
+
+// document.write("El primer elemento es "+ciudades[0]+",");
+// document.write("El tercer elemento es "+ciudades[2]+",");
+// document.write("El ultimo elemento es "+ciudades[ciudades.length-1]+",")
+// document.write("<br>");
+// document.write("el elemento que ocupa la 2da posicion es"+ciudades[3]);
+
+
+
+
 
